4ed9a6e1dde65f48923c02b2119064e5fb77563f
Arashinera <burtnyk.yarik@gmail.com>
2024-04-02 07:53:29 +0300

    Update variables, Update comments

--- src/Variables/MainClass.java
+++ src/Variables/MainClass.java
@@ -7,7 +7,7 @@ import java.util.Locale;
 
 public class MainClass {
 
-    //Створюємо постійні ціни на товар
+    //Створюємо константи :
     private static final double SMARTPHONE_PRICE = 305.99;
     private static final double LAPTOP_PRICE = 570.95;
     private static final String CURRENCY = "EUR";
@@ -18,30 +18,33 @@ public class MainClass {
         Scanner input = new Scanner(System.in);
         input.useLocale(Locale.ENGLISH);
 
-        System.out.println("Order №1. Please, tell me your name :");
-        String name = input.nextLine();
-        System.out.println("What type of product do you want to buy?  :");
-        String product = input.nextLine();
-        System.out.println("What's your address street?");
-        String street = input.nextLine();
-        System.out.println("What's your house number?");
-        int house = input.nextInt();
+        String userName, productName, street;
+        int houseNumber;
 
-        System.out.println("\nOrder №1.\nClient : " + name + ".\nProduct : " + product + ", price " + CURRENCY + SMARTPHONE_PRICE + ".\nAddress : " + street + ", " + house + ".\n");
+        System.out.println("Order №1\nPlease, tell me your name :");
+        userName = input.nextLine();
+        System.out.println("What type of product do you want to buy?  :");
+        productName = input.nextLine();
+        System.out.println("What's your address street?");
+        street = input.nextLine();
+        System.out.println("What's your house number?");
+        houseNumber = input.nextInt();
+
+        System.out.println("\nOrder №1\nClient : " + userName + ".\nProduct : " + productName + ", price " + CURRENCY + SMARTPHONE_PRICE + ".\nAddress : " + street + ", " + houseNumber + ".\n");
 
         //Очищуємо наш сканер аби після int не було перешкод :
         input.nextLine();
 
-        System.out.println("Order №2. Please, tell me your name :");
-        name = input.nextLine();
+        System.out.println("Order №2\nPlease, tell me your name :");
+        userName = input.nextLine();
         System.out.println("What type of product do you want to buy? :");
-        product = input.nextLine();
+        productName = input.nextLine();
         System.out.println("What's your address street?");
         street = input.nextLine();
         System.out.println("What's your house number?");
-        house = input.nextInt();
+        houseNumber = input.nextInt();
 
-        System.out.println("\nOrder №2.\nClient : " + name + ".\nProduct : " + product + ", price " + CURRENCY + LAPTOP_PRICE + ".\nAddress : " + street + ", " + house + ".");
+        System.out.println("\nOrder №2\nClient : " + userName + ".\nProduct : " + productName + ", price " + CURRENCY + LAPTOP_PRICE + ".\nAddress : " + street + ", " + houseNumber + ".");
 
     }
 }
