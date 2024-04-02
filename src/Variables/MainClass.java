9a3267ac93f0e3bf0eee6e7f39a114368840048f
Arashinera <burtnyk.yarik@gmail.com>
2024-04-02 07:35:36 +0300

    Initial Commit

---
+++ src/Variables/MainClass.java
@@ -0,0 +1,47 @@
+package Variables;
+
+//Добавимо нову бібліотеку Scanner у наш клас. Вона надає можливість використовувати сканер для вводу даних :
+import java.util.Scanner;
+
+public class MainClass {
+
+    //Створюємо постійні ціни на товар
+    private static final double SMARTPHONE_PRICE = 305.99;
+    private static final double LAPTOP_PRICE = 570.95;
+
+    public static void main(String[] args) {
+
+        //Налаштовуємо сканер
+        Scanner input = new Scanner(System.in);
+
+        //Створюємо перше замовлення
+        System.out.println("Order №1. Please, tell me your name :");
+        String name = input.nextLine();
+        System.out.println("What type of product do you want to buy?  :");
+        String product = input.nextLine();
+        System.out.println("What's your address street?");
+        String street = input.nextLine();
+        System.out.println("What's your house number?");
+        int house = input.nextInt();
+
+        System.out.println("\nOrder №1.\nClient : " + name + ".\nProduct : " + product + ", price EUR " + SMARTPHONE_PRICE + ".\nAddress : " + street + ", " + house + ".\n");
+
+        //Очищуємо наш сканер аби після int не було перешкод.
+        input.nextLine();
+
+        //Створюємо друге замовлення використовуючи ті ж самі змінні аби не збільшувати напругу для швидкодії програми.
+        System.out.println("Order №2. Please, tell me your name :");
+        name = input.nextLine();
+        System.out.println("What type of product do you want to buy? :");
+        product = input.nextLine();
+        System.out.println("What's your address street?");
+        street = input.nextLine();
+        System.out.println("What's your house number?");
+        house = input.nextInt();
+
+        //Виводимо інформацію о замовленнях :
+
+        System.out.println("\nOrder №2.\nClient : " + name + ".\nProduct : " + product + ", price EUR " + LAPTOP_PRICE + ".\nAddress : " + street + ", " + house + ".");
+
+    }
+}
