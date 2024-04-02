7347b43bec1fdcaa911a4dfebac353fffd9c07f3
Arashinera <burtnyk.yarik@gmail.com>
2024-04-02 07:44:23 +0300

    Add const, Add Locale, Update comments

--- src/Variables/MainClass.java
+++ src/Variables/MainClass.java
@@ -1,20 +1,23 @@
 package Variables;
 
 //Добавимо нову бібліотеку Scanner у наш клас. Вона надає можливість використовувати сканер для вводу даних :
+
 import java.util.Scanner;
+import java.util.Locale;
 
 public class MainClass {
 
     //Створюємо постійні ціни на товар
     private static final double SMARTPHONE_PRICE = 305.99;
     private static final double LAPTOP_PRICE = 570.95;
+    private static final String CURRENCY = "EUR";
 
     public static void main(String[] args) {
 
-        //Налаштовуємо сканер
+        //Налаштовуємо сканер та локейл :
         Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.ENGLISH);
 
-        //Створюємо перше замовлення
         System.out.println("Order №1. Please, tell me your name :");
         String name = input.nextLine();
         System.out.println("What type of product do you want to buy?  :");
@@ -24,12 +27,11 @@ public class MainClass {
         System.out.println("What's your house number?");
         int house = input.nextInt();
 
-        System.out.println("\nOrder №1.\nClient : " + name + ".\nProduct : " + product + ", price EUR " + SMARTPHONE_PRICE + ".\nAddress : " + street + ", " + house + ".\n");
+        System.out.println("\nOrder №1.\nClient : " + name + ".\nProduct : " + product + ", price " + CURRENCY + SMARTPHONE_PRICE + ".\nAddress : " + street + ", " + house + ".\n");
 
-        //Очищуємо наш сканер аби після int не було перешкод.
+        //Очищуємо наш сканер аби після int не було перешкод :
         input.nextLine();
 
-        //Створюємо друге замовлення використовуючи ті ж самі змінні аби не збільшувати напругу для швидкодії програми.
         System.out.println("Order №2. Please, tell me your name :");
         name = input.nextLine();
         System.out.println("What type of product do you want to buy? :");
@@ -39,9 +41,7 @@ public class MainClass {
         System.out.println("What's your house number?");
         house = input.nextInt();
 
-        //Виводимо інформацію о замовленнях :
-
-        System.out.println("\nOrder №2.\nClient : " + name + ".\nProduct : " + product + ", price EUR " + LAPTOP_PRICE + ".\nAddress : " + street + ", " + house + ".");
+        System.out.println("\nOrder №2.\nClient : " + name + ".\nProduct : " + product + ", price " + CURRENCY + LAPTOP_PRICE + ".\nAddress : " + street + ", " + house + ".");
 
     }
 }
